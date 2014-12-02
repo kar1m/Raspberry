40672fa3792dfc93837832e13b41385eb22dbcbe
Benhmida Karim <kbenhmida@if501-219-21.insa-lyon.fr>
2014-12-02 09:05:45 +0100

    maj

--- kernel.c
+++ kernel.c
@@ -1,46 +1,27 @@
-#include "stdlib.h"
-#include "sched.h"
 #include "hw.h"
+#include "sched.h"
 
-void funcA()
-{
-    int cptA = 0;
-    while ( 1 ) {
-        cptA ++;
-        ctx_switch();
-    }
+void funcA() {
+  int cptA = 0;
+  while(1) {
+    cptA ++;
+  }
 }
 
-void funcB()
-{
-    int cptB = 1;
-    while ( 1 ) {
-        cptB += 2 ;
-        ctx_switch();
-    }
-}
-
-void funcC(int babar)
-{
-    while(babar<37)
-    {
-        babar++;
-        ctx_switch();
-    }
+void funcB() {
+  int cptB = 1;
+  while(0) {
+    cptB += 2 ;
+  }
 }
 
 //------------------------------------------------------------------------
-int kmain ( void )
-{
-    init_hw();
-    current_process->pt_fct = NULL;
-    int babar = 33;
-    int stack_size = STACK_SIZE;
-    create_process(funcB, NULL, stack_size);
-    create_process(funcA, NULL, stack_size);
-    create_process(funcC, babar, stack_size);
-    start_sched();
-    ctx_switch();
-    /* Pas atteignable vues nos 2 fonctions */
-    return 0;
+
+int kmain(void) {
+  init_hw();
+  create_process(funcB, NULL, STACK_SIZE);
+  create_process(funcA, NULL, STACK_SIZE);
+  start_sched();
+  /*Pas atteignable vues nos 2 fonctions */
+  return 0;
 }
